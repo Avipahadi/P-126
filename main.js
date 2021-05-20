d2ebb41532af3dac17d568e4074f95d7c1569910
Arnav Sharma <68996768+Avipahadi@users.noreply.github.com>
2021-05-20 16:24:16 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var leftWristX = 0;
 var leftWristY = 0;
 var rightWristX = 0;
 var rightWristY = 0;
+var scoreleftwrist = 0;
 
 
 function preload() {
@@ -23,6 +24,8 @@ function setup() {
 
 function gotPoses(results) {
     if (results.length > 0) {
+        scoreleftwrist = results[0].pose.keypoints[9].score;
+        console.log("Score left wrist = " + scoreleftwrist)
         console.log(results);
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
@@ -39,4 +42,18 @@ function modelLoaded() {
 
 function draw() {
     image(video, 0, 0, 600, 500);
+    fill("#FF0000");
+    stroke("#ba0000");
+    if (scoreleftwrist > 0.2) {
+        if (song_1.isPlaying() == true) {
+            console.log("Already Playing")
+        } else {
+            song_1.play();
+            song_2.stop();
+            song_1.setVolume(1000);
+            song_2.setVolume(0);
+            circle(leftWristY, leftWristX, 20);
+            document.getElementById("song_name").innerHTML = 'Song = Main tera boyfriend';
+        }
+    }
 }
